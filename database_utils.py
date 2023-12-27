2ba185e39509a705320dfc40c702669f16ddfad8
Abhik Mukhopadhyay <abhikuohyd@gmail.com>
2023-12-27 04:17:29 +0000

    adding yaml file

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,12 @@
+import yaml
+
+
 class DatabaseConnector:
+
+    def read_db_creds(self, cred_file):
+        with open(cred_file, 'r') as file:
+            data = yaml.safe_load(file)
+            return data
+
+
     pass
