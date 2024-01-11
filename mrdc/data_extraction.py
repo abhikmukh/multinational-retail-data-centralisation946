8f1bc4302841a8321e1f3b8b2ec965083c491055
Abhik Mukhopadhyay <abhikuohyd@gmail.com>
2024-01-11 13:59:43 +0000

    new folder structure

--- mrdc/data_extraction.py
+++ mrdc/data_extraction.py
@@ -22,13 +22,6 @@ class DataExtractor:
 
 
 
-data_connector = DatabaseConnector\
-    (base_path="C:\Users\abhik\Documents\aicore\multinational-retail-data-centralisation946",
-     cred_file="db_creds.yaml")
-
-data_extractor = DataExtractor()
-data_df = data_extractor.read_rds_table(data_connector, table_name="customer")
-
 
 
 
